76f203bc5793793d47a6b78a5ff44fce2ee0205d
khurram <ksk2431990@gmail.com>
2024-10-25 23:23:11 +0500

    kb 1st commit

---
+++ cypress/e2e/Dummy.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress"/>
+
+
+describe("This is dummy test",()=>{
+
+
+ it("this is first test",()=>{
+
+
+  cy.visit("https://chatgpt.com/?ref=dotcom");
+
+
+
+})
+
+
+
+
+
+})
