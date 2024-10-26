e1b01bcfce69bfd4ff7c3b75ea3c10b202b40b07
khurram <ksk2431990@gmail.com>
2024-10-26 10:20:25 +0500

    3rd commit

---
+++ cypress/e2e/Kb.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress"/>
+
+
+describe("This is dummy test",()=>{
+
+
+    it("this is first test",()=>{
+   
+   
+     cy.visit("https://chatgpt.com/?ref=dotcom");
+   
+   
+   
+   })
+   it("this is 2nd test",()=>{
+   
+   
+     cy.visit("https://chatgpt.com/?ref=dotcom");
+     cy.log("this is 2nd test")
+   
+   
+   
+   
+   })
+   
+   
+   
+   
+   })
